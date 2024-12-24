f27ae281136c212125e0cc6ed6dcc83400846856
Your Name <you@example.com>
2024-12-24 21:26:53 +0100

    fetch

--- routes/classementRoutes.js
+++ routes/classementRoutes.js
@@ -74,6 +74,9 @@ async function fetchPage(url) {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
                 'Accept-Language': 'en-US,en;q=0.9',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Referer': 'https://www.google.com/',
+                'Connection': 'keep-alive',
             },
         });
         return response.data;
